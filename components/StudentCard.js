c49f2b1b77c70c143cb95ce2352f48d3c4da6b74
ixeaxm <omaom5691@gmail.com>
2025-12-09 22:31:07 +0700

    op

--- components/StudentCard.js
+++ components/StudentCard.js
@@ -1,21 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import styles from '../assets/styles/studentCard'
+import styles from '../assets/styles/StudentCardStyles'
 
-const StudentCrad = (props) => {
+const StudentCard = (props) => {
   return (
-    <View style={styles.crad}>
-        <View style={styles.avatarContainer}> 
+    <View style={styles.card}>
+        <View style={styles.avatarContainer}>
             <Text style={styles.avatar}>{props.emoji}</Text>
-
         </View>
-        <Text style={styles.studentName}> {props.name}</Text>
-        <Text style={styles.studentClass}> {props.class} </Text>
+        <Text style={styles.studentName}>{props.name}</Text>
+        <Text style={styles.studentClass}>{props.class}</Text>
         <Text style={styles.studentAge}>{props.age}</Text>
-        <Text style={styles.studentHobby}>{props.hobby} </Text>
-
+        <Text style={styles.studentHobby}>{props.hobby}</Text>
     </View>
   )
 }
 
-export default StudentCrad
+export default StudentCard
